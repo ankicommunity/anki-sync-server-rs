0465cd0947b28d077547ccf961251f36d613bc17
dobefore <1432338032@qq.com>
2021-10-28 18:12:23 +0800

    gen settings if not exist

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 custom sync server implemented in Rust.
 ### download binary package
 ### build from source
+
 ### TODO
 - add ssl certificate verify
 - error handle
 - anki versions test
+- automatically add user account into auth ab when account
+is not empty in Settings.toml
+- add shell script for  build from source on linux and windows
+- make deb for linux
 
 ### REFERENCE
 ankisyncd architecture or apis depend on [ankicommunity/anki-sync-server](https://github.com/ankicommunity/anki-sync-server) and
--- src/bin/ankisyncd.rs
+++ src/bin/ankisyncd.rs
@@ -4,11 +4,31 @@ use ankisyncd::{
     session::SessionManager,
     sync::{favicon, sync_app, welcome},
 };
-
+use std::{path::Path,fs};
 use env_logger;
 use std::sync::Mutex;
+/// generate Setting.toml if not exist
+fn setting_exist() {
+    let p=Path::new("Settings.toml");
+    let content=r#"
+host="0.0.0.0"
+port = "27701"
+data_root = "./collections"
+base_url = "/sync/"
+base_media_url = "/msync/"
+auth_db_path = "./auth.db"
+session_db_path = "./session.db"
+# following fields will be added into auth.db if not empty
+username=""
+password=""
+    "#;
+    if !p.exists() {
+        fs::write(&p, content).unwrap();
+    }
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+   setting_exist();
     std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
     env_logger::init();
     //reference py ver open col
