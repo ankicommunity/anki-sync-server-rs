95f273ea9f08c9f60a9ce18fc0f2db3c99542fc9
dobefore <1432338032@qq.com>
2021-12-12 13:08:09 +0800

    up cc.sh auto-compress after build add config file for cc'use

--- cc.sh
+++ cc.sh
@@ -1,23 +1,23 @@
 # for cross compile use
 
-# enter into dir anki=server-rs
-cd anki-sync-server-rs
-# for aarch64
-echo "cross-compile for aarch64"
-file1="target/aarch64-unknown-linux-musl/release/ankisyncd"
-if [ -f $file1 ];then
- echo "$file1 exists"
-else
-# add env var
-export PATH="$HOME/aarch64-linux-musl-cross/bin:$PATH"
-# use compiled openssl
-export OPENSSL_LIB_DIR=/home/ubuntu/openssl_aarch64_musl
-export OPENSSL_INCLUDE_DIR=/home/ubuntu/openssl_aarch64_musl/include
-export OPENSSL_STATIC=true
-# cross build for aarch64
-cargo build --target=aarch64-unknown-linux-musl --release
-fi
+# default current dir anki=server-rs/
 
+# for aarch64
+# echo "cross-compile for aarch64"
+# file1="target/aarch64-unknown-linux-musl/release/ankisyncd"
+# if [ -f $file1 ];then
+#  echo "$file1 exists"
+# else
+# # add env var
+# export PATH="$HOME/aarch64-linux-musl-cross/bin:$PATH"
+# # use compiled openssl
+# export OPENSSL_LIB_DIR=/home/ubuntu/openssl_aarch64_musl
+# export OPENSSL_INCLUDE_DIR=/home/ubuntu/openssl_aarch64_musl/include
+# export OPENSSL_STATIC=true
+# # cross build for aarch64
+# cargo build --target=aarch64-unknown-linux-musl --release
+# fi
+version="0.1.3"
 # for armv7
 echo "cross-compile for armv7"
 file2="target/armv7-unknown-linux-musleabihf/release/ankisyncd"
@@ -35,15 +35,27 @@ export OPENSSL_INCLUDE_DIR=/home/ubuntu/openssl-armv7/include
 export OPENSSL_STATIC=true
 # cross build for armv7
  cargo build --target armv7-unknown-linux-musleabihf --release
+
+mkdir ankisyncd-arm
+cp target/armv7-unknown-linux-musleabihf/release/ankisyncd ankisyncd-arm
+cp Settings.toml ankisyncd-arm
+tar -czvf ankisyncd-$version-arm.tar.gz ankisyncd-arm
+mv ankisyncd-$version-arm.tar.gz ~
 fi
 
 #for x86-64
 echo "cross-compile for x64"
 # add env var
 export PATH="$HOME/x86_64-linux-musl-cross/bin:$PATH"
+export CC=
 
-openssl_x64
 export OPENSSL_LIB_DIR=/home/ubuntu/openssl_x64
 export OPENSSL_INCLUDE_DIR=/home/ubuntu/openssl_x64/include
 export OPENSSL_STATIC=true
 cargo build --release --target=x86_64-unknown-linux-musl
+
+mkdir ankisyncd-linux
+cp target/x86_64-unknown-linux-musl/release/ankisyncd ankisyncd-linux
+cp Settings.toml ankisyncd-linux
+tar -czvf ankisyncd-$version-linux.tar.gz ankisyncd-linux
+mv ankisyncd-$version-linux.tar.gz ~
