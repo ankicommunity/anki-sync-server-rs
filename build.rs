63d439808e406c52798980d867090f9e0595c683
dobefore <1432338032@qq.com>
2022-02-20 12:01:24 +0800

    cargo fmt

--- build.rs
+++ build.rs
@@ -3,6 +3,6 @@ fn main() {
     let pat = "rustls";
     let key = format!("CARGO_FEATURE_{}", pat).to_uppercase();
     if env::var_os(key).is_some() {
-           println!("{}", format!("cargo:rustc-cfg=feature=\"{}\"", pat))
-            }
+        println!("{}", format!("cargo:rustc-cfg=feature=\"{}\"", pat))
+    }
 }
