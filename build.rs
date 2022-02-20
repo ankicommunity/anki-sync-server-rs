d3b36499ba70cef2dc8ecb64d9cf5b9c487499a7
dobefore <1432338032@qq.com>
2022-02-20 11:55:55 +0800

    clippy code

--- build.rs
+++ build.rs
@@ -2,10 +2,7 @@ use std::env;
 fn main() {
     let pat = "rustls";
     let key = format!("CARGO_FEATURE_{}", pat).to_uppercase();
-    match env::var_os(key) {
-        Some(_) => {
-            println!("{}", format!("cargo:rustc-cfg=feature=\"{}\"", pat))
-        }
-        None => {}
-    }
+    if env::var_os(key).is_some() {
+           println!("{}", format!("cargo:rustc-cfg=feature=\"{}\"", pat))
+            }
 }
