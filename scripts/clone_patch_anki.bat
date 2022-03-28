f86c42d5be93fd2c67f9e9a938145f6fa0ea5bc5
dobefore <1432338032@qq.com>
2022-03-28 10:54:54 +0800

    update script on patch anki

--- scripts/clone_patch_anki.bat
+++ scripts/clone_patch_anki.bat
@@ -1,4 +1,4 @@
-@REM the file was created by @redmie
+@REM the file was created by @dobefore and @redmie
 @REM following line command will not echo every line command
 @echo off
 @REM Allow external definition of anki repository URL
@@ -7,6 +7,11 @@ GOTO END
 :NOURLSET
 set ANKI_REPO_URL="https://github.com/ankitects/anki"
 :END
+
+@REM here e.g. D:\software\vscode_project\anki_sync\anki-sync-server-rs
+set PROJECT_ROOT= %CD%
+set ANKI_PATCH_FOLDER=%PROJECT_ROOT%\anki_patch
+set ANKI_FILE_SUFFIX=_anki_rslib.patch
 
 @REM Set up other variables
 set ANKI_TAG=2.1.46
@@ -20,3 +25,26 @@ echo "Checking out commit %ANKI_COMMIT% and applying patch"
 cd anki
 git checkout %ANKI_COMMIT%
 git apply %ANKI_PATCH_FOLDER%\%ANKI_COMMIT%%ANKI_FILE_SUFFIX%
+
+
+@REM How to create a patch file
+
+@REM clone anki repo
+
+@REM get original commit ID 5dab7ed47ec6d17226d2fc0529c32a56e40e5f8a
+@REM git rev-parse HEAD
+
+@REM make changes to anki lib ,e.g.add pub to structs...
+@REM git commit
+
+@REM get current commit ID  480a572137a51316bab2d97f2435cdfe328c462c
+@REM git rev-parse HEAD
+
+@REM use current commit ID to patch
+@REM git format-patch 480a572137a51316bab2d97f2435cdfe328c462c -1
+
+@REM you can rename patch file with original commit ID
+@REM ren .\0001-create-patch.patch 5dab7ed47ec6d17226d2fc0529c32a56e40e5f8a_anki_rslib.patch
+
+@REM last and not least, convert CRLF in patch if it is to LF using dos2unix
+@REM dos2unix %ANKI_PATCH_FOLDER%\%ANKI_COMMIT%%ANKI_FILE_SUFFIX%
