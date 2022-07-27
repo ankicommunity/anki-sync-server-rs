195e73b91ffe8e6c2fed95435a9e8314485e8b04
sagit <36596628+Sagit-chu@users.noreply.github.com>
2022-07-27 18:39:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ ! -f /app/ankisyncd.toml ]; then
-	mv /ankisyncd.toml /app/ankisyncd.toml
+	cp -u /ankisyncd.toml /app/ankisyncd.toml
 fi
 /usr/local/bin/ankisyncd -c /app/ankisyncd.toml
