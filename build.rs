1f08ec5d6bdd1591d63a95a94bbee90a5d2a6d4d
dobefore <46564102+dobefore@users.noreply.github.com>
2022-11-20 12:21:50 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,18 @@
 use std::env;
 fn main() {
+     // should consider native build on arm platform
+
+    // used in cross compile while building with CD
+    // such as arm-unknown-linux-musleabihf
+    let target = env::var("TARGET").expect("TARGET was not set");
+    if target.contains("arm") && target.contains("musl") {
+        // find and link static sqlite3 lib
+        let sql = Path::new(&env::current_dir().unwrap()).join("sql/lib");
+        println!("cargo:rustc-link-search=native={}", sql.display());
+        println!("cargo:rustc-link-lib=static=sqlite3");
+    }
+
+    
     let pat = "tls";
     let key = format!("CARGO_FEATURE_{}", pat).to_uppercase();
     if env::var_os(key).is_some() {
