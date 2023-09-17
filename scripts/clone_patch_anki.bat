49e1488fec38c7a9549b8795b48a41e476a70479
Dominique Martinet <asmadeus@codewreck.org>
2023-09-17 19:49:05 +0900

    Update for anki 23.10
    anki 23.10 includes protocol changes (see linked PR, and others) so we
    need to update the anki base commit and patch.
    
    I've aligned the rusqlite dependency to fix rust build, and while I was
    it aligned a few more where rust would be building an extra version of
    the lib for anki.
    
    Link: https://github.com/ankitects/anki/pull/2647

--- scripts/clone_patch_anki.bat
+++ scripts/clone_patch_anki.bat
@@ -15,7 +15,7 @@ set ANKI_FILE_SUFFIX=_anki_rslib.patch
 
 @REM Set up other variables
 set ANKI_TAG=2.1.46
-set ANKI_COMMIT=d9d36078f17a2b4b8b44fcb802eb274911ebabe7
+set ANKI_COMMIT=fac9e0ee1436ba5ac3366c72dd9394a6e692b1cf
 
 @REM Clone & patch
 echo "Cloning anki from %ANKI_REPO_URL%"
